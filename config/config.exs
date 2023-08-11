73f4043db0d580694ad502f60390bcc634e41238
Nick Hallstrom <nick@trycrew.com>
2023-08-11 13:59:56 -0600

    Update default config

--- config/config.exs
+++ config/config.exs
@@ -1,26 +1,4 @@
 import Config
 
 # Default config for tests
-config :safe_inspect,
-  redacted_keys: [
-    :birth_date,
-    :dob,
-    :driver_license_number,
-    :email,
-    :ethnicity,
-    :first_name,
-    :full_name,
-    :gender,
-    :ip_address,
-    :last_name,
-    :nationality,
-    :passport,
-    :passport_number,
-    :password,
-    :phone,
-    :phone_number,
-    :salary,
-    :social_security_number,
-    :ssn,
-    :username
-  ]
+config :safe_inspect, redacted_keys: [:birth_date, :dob]
